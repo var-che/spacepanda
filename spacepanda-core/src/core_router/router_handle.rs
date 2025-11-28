a03376314d112363128127bbd4e95c99a2094119
varche <filo.vlada@gmail.com>
2025-11-28 23:20:41 +0100

    router_handler doc

---
+++ spacepanda-core/src/core_router/router_handle.rs
@@ -0,0 +1,21 @@
+/*
+   RouterHandle
+   Single enty point for the rest of the application to interact with the router.
+   It exposes concise async methods for starting/stopping the router, sending/receiving messages,
+   and managing connections.
+
+   E.G: send_direct(peer_id, bytes) -> Result<()>
+        send_anonymous(peer_id, bytes) -> Result<()>
+        broadcast(topic, bytes) -> Result<()>
+        subscribe(topic) -> Stream<InnerEnvelope>
+        rpc_call(peer_id, method, params) -> Result<response_bytes>
+
+    Workflow and where it sits?
+
+    It is called by the APP (MLS layer), to send encrypted payloads to other peers.
+    Internally it packages a RouterCommand and pushes it onto the router actor main channel.
+    And it awaits responses for RPC (correlated by request ID).
+
+    Example: `send_anonymous(peer_id, mls_cyphertext)` will create a `RouterCommand::OverlaySend { dest:peer_id, payload }`
+    
+*/
